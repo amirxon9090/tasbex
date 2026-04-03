552e7b79ef8f7f155acd9c9096e632fc449d9feb
amirxon9090 <amirxoneshmuratov711@gmail.com>
2026-04-03 16:21:33 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+let count = 0;
+
+
+function updateCount() {
+  document.getElementById("count").innerHTML = count;
+}
+
+
+function increaseCount() {
+  count++;
+  localStorage.setItem("count", count);
+  updateCount();
+}
+
+  
+
+
+function decreaseCount() {
+if (count > 0)
+
+  count--;
+  localStorage.setItem("count", count);
+  updateCount();
+
+
+  
+
+  
+}
+
+
+function resetCount() {
+  count = 0;
+  localStorage.setItem("count", count);
+  updateCount();
+}
+
+window.onload = function(){ 
+    let saved = localStorage.getItem("count")
+    if (saved !== null){
+        count = Number(saved)
+    }
+      updateCount();
+};
+
